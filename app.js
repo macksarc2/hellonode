25b9cd6e3f6e67fc45de22bfdffb830b6fca098a
macksamr <macksamr@gmail.com>
2024-08-17 13:14:47 -0500

    nginx ingress

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const os = require('os');
 const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-  res.send('Hello World!');
+  const now = new Date();
+  const currentTime = now.toLocaleTimeString();
+  res.send(`Hello World! ${currentTime} UTC ${os.hostname()}`);
 });
 
 app.listen(port, () => {
